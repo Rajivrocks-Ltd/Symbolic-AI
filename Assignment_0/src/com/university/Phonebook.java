a9b7abdb9a6675890f05a6842f028f2e9472baa9
Rajiv Jethoe <y.jethoe@hotmail.com>
2022-09-18 04:29:19 +0200

    Added more comments and some error handling

--- Assignment_0/src/com/university/Phonebook.java
+++ Assignment_0/src/com/university/Phonebook.java
@@ -12,12 +12,18 @@ public class Phonebook {
         people = new Vector<>();
     }
 
+    // File scanner that reads contents of a file and tries to add contents to the "people" vector
     public void read(String file) {
         try {
             Scanner scanner = new Scanner(new File(file));
             while (scanner.hasNextLine()) {
                 String[] person = scanner.nextLine().split(",");
-                people.addElement(new Person(person[0].strip().toLowerCase(), person[1].strip()));
+                try {
+                    people.addElement(new Person(person[0].strip().toLowerCase(), person[1].strip()));
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("A problem occurred while adding a person to the DB. Probable" +
+                            " cause: malformed input, check input!");
+                }
             }
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -25,6 +31,7 @@ public class Phonebook {
         }
     }
 
+    // Find a number of a person given their full name.
     public String number(String name) {
         for (Person p: people) {
             if (Objects.equals(p.name, name.toLowerCase())) { return p.number; }
@@ -42,13 +49,13 @@ public class Phonebook {
         //Initiate a List which we will work with for the recursive steps.
         List<String> names = new ArrayList<>();
 
-        // convert vector to list so we can work with this in the recursive funtion.
+        // convert vector to list, so we can work with this in the recursive function.
         for(Person p: tosort) {
             names.add(p.name + "-" + p.number);
         }
 
-        // Recursive function, size of the function minus 1 since .size is not null based length.
-        names = sortString(names, names.size()-1);
+        // Recursive function, size of the function minus 1 since .size is not a null based length.
+        names = sortStringList(names, names.size()-1);
 
         // Convert list back to vector for the final step.
         sorted = listToVector(names);
@@ -68,7 +75,7 @@ public class Phonebook {
     }
 
     // Main recursion loop
-    private List<String> sortString(List<String> names, int n) {
+    private List<String> sortStringList(List<String> names, int n) {
 
         /*
             Make two variables, boolean to keep track if the list is sorted
@@ -84,7 +91,7 @@ public class Phonebook {
         for(int i = 0; i < n; i++){
             if(!isSmaller(names.get(i), names.get(i+1))) {
                 /*
-                    Firstly, the second item selected item will be stored in a temp variable.
+                    Firstly, the second selected item will be stored in a temp variable.
                     Secondly, The first selected item is set to the index of the second item.
                     Thirdly, the temp value which is the second selected item, is set to the index of the first item.
                  */
@@ -92,26 +99,35 @@ public class Phonebook {
                 names.set(i + 1, names.get(i));
                 names.set(i, temp);
 
-                // if in the for loops means it's still not sorted, that's why: false.
+                // if we are in the for loop it means it's still not sorted, that's why: false.
                 sorted = false;
             }
         }
         // Recursive call, insert the list + the length of the list minus 1, so it will not endlessly run the loop.
         if(sorted) { return names; }
-        return sortString(names, n-1);
+        return sortStringList(names, n-1);
     }
 
     private boolean isSmaller(String s1, String s2) {
-        // checks if s1 is smaller than s2, alphabetically
-        int limit = Math.min(s1.length(), s2.length());
+        /*
+            checks if s1 is smaller than s2, alphabetically
+            get the smallest string as the limit for the for loop.
+            Loop over the first and subsequent letter of each word and compare values of each letter.
+            If s1n[i] is bigger than s2n[i] than name 1 is bigger, and we return false
+            If the opposite is true we return true
+            if we run out of characters we evaluate s1n and s2n once more on bases of their entire length -
+            shortest string will be the smaller name in this case.
+         */
         String s1n = s1.split("-")[0];
         String s2n = s2.split("-")[0];
+
+        int limit = Math.min(s1n.length(), s2n.length());
 
         for(int i = 0; i < limit; i++){
             if(s1n.charAt(i) > s2n.charAt(i)) { return false; }
             else if(s1n.charAt(i) < s2n.charAt(i)) { return true; }
         }
-        return true;
+        return s1n.length() < s2n.length();
     }
 
     @Override
